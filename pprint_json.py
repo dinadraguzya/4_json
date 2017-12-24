47a1b1a73d7cab84402f47f678e7f3b4316e3e2f
Dina <dina.draguzya@gmail.com>
2017-12-24 20:38:11 +0200

    Added file path check

--- pprint_json.py
+++ pprint_json.py
@@ -1,15 +1,20 @@
 import json
 import sys
+import os
 
 
-def load_data(filepath):
-    with open(filepath, 'r') as opened_file:
+def load_data(file_path):
+    with open(file_path, 'r') as opened_file:
         return json.load(opened_file)
 
 
-def pretty_print_json(json_file):
-    print(json.dumps(json_file, ensure_ascii=False, indent=4))
+def pretty_print_json(json_content):
+    print(json.dumps(json_content, ensure_ascii=False, indent=4))
 
 
 if __name__ == '__main__':
-    pretty_print_json(load_data(sys.argv[1]))
+    file_path = sys.argv[1]
+    if os.path.exists(file_path) and os.path.isfile(file_path):
+        pretty_print_json(load_data(file_path))
+    else:
+        print("The file doesn't exist!")
