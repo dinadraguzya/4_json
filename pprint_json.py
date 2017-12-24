c23f1ef3820bc9707a70bd7f083afc69e7373bf0
Dina <dina.draguzya@gmail.com>
2017-12-24 13:42:37 +0200

    Added functions to read, format and print data from a file

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,15 @@
 import json
+import sys
 
 
 def load_data(filepath):
-    pass
+    with open(filepath, 'r') as opened_file:
+        return json.load(opened_file)
 
 
-def pretty_print_json(data):
-    pass
+def pretty_print_json(json_file):
+    print(json.dumps(json_file, ensure_ascii=False, indent=4))
 
 
 if __name__ == '__main__':
-    pass
+    pretty_print_json(load_data(sys.argv[1]))
